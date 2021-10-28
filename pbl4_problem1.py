f01490c2f73ab731126d073abf02270cd0aa7f75
Sarah Sanders <sarah.sanders@ucl.ac.uk>
2021-10-28 08:04:05 +0100

    Update pbl4_problem1.py
    updated the url and made it clearer where the data set is in the project structure

--- pbl4_problem1.py
+++ pbl4_problem1.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 if __name__ == '__main__':
     ''' 
-    Add a line of code to create a dataframe by reading the file from csv
+    Add a line of code to create a dataframe by reading the file ('data/paralympics_raw.csv') from csv
     If you don't have the dataset saved in your project use the URL:
     https://raw.githubusercontent.com/nicholsons/comp0035_pbl4/master/data/paralympics_raw.csv?token=AHBUVRXMMNPI7VSWY4P34IDBO2TES'
     '''
