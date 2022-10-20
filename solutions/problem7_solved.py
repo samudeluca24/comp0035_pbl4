e0d1e2fe8cd2390e6b0ad811425bb3c60c836cde
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2022-10-20 08:40:58 +0100

    updated pbl instructions

--- solutions/problem7_solved.py
+++ solutions/problem7_solved.py
@@ -11,7 +11,6 @@ def prepare_data():
             Problem 4: Removes the rows with nulls in Participants columns; and replaces missing values in Type column
             Problem 5: Removes the trailing spaces from the text entries in the Type column
             Problem 6: Merges the NOC code column from the noc_csv and adds missing values
-            Problem 7: Add a duration column that calculates the difference between the start and end dates
         :return: None
     """
     # Problem 1. Load the data and set pandas display options to fit all rows and columns
@@ -27,41 +26,59 @@ def prepare_data():
     # Problem 5: Removes the trailing spaces from the text entries in the Type column
     df['Type'] = df['Type'].str.strip()
     # Problem 6: Merge the NOC code column from the noc_csv
-    # Open the noc_regions.csv in a dataframe
+    # Create a data frame with the NOC regions
     noc_csv = Path(__file__).parent.parent.joinpath('data', 'noc_regions.csv')
     df_noc = pd.read_csv(noc_csv)
     # Drop the 'notes' column
-    df_noc = df_noc.drop(['notes'], axis=1)
+    df_noc.drop(['notes'], axis=1, inplace=True)
     # Merge the columns where df['Country'] matches df_noc['region']
     df_merged = df.merge(df_noc, how='left', left_on='Country', right_on='region')
-    df_merged = df_merged.drop(['region'], axis=1)
+    df_merged.drop(['region'], axis=1, inplace=True)
     # Manually add the 'NOC' code for Great Britain (GBR) and Republic of Korea (KOR)
-    # You need to replace a value in one column based on a condition in another.
-    # There will be more than one way to do this, the following uses a mask (condition).
-    df_merged = df_merged['NOC'].mask(df_merged['Country'] == 'Great Britain', 'GBR')
-    df_merged = df_merged['NOC'].mask(df_merged['Country'] == 'Republic of Korea', 'KOR')
-    # Problem 7: Add a column to calculate duration
-    # Replace the start and end columns by combining the Start & Year, and End & Year columns
-    df_merged["Start"] = df_merged["Start"] + '-' + df_merged["Year"].astype(str)
-    df_merged["End"] = df_merged["End"] + '-' + df_merged["Year"].astype(str)
-    # Change the column datatype to date-time format
-    # Pandas to_datetime  handles most date formats so you can run the following without the format= and it will work
-    # date time formats are here for reference https://docs.python.org/3/library/datetime.html
-    df_merged['Start'] = pd.to_datetime(df_merged['Start'], format='%d-%b-%Y')
-    df_merged['End'] = pd.to_datetime(df_merged['End'])
-    # Create a duration column that calculates days between the start and end
-    df_merged['Duration'] = df_merged['End'] - df_merged['Start']
-    # The output of the above is in timedelta format, however we want to compare duration as int
-    # Convert the format
-    df_merged['Duration'] = df_merged['Duration'].dt.days.astype('int')
-    # Save the file
-    prepared_csv_filepath = Path(__file__).parent.parent.joinpath('data', 'paralympics_prepared.csv')
-    df_merged.to_csv(prepared_csv_filepath, index=False)
+    # This is a little more tricky as you need to replace a value in one column based on a condition in another
+    # There will be more than one way to do this, I used a mask (condition).
+    df_merged['NOC'].mask(df_merged['Country'] == 'Great Britain', 'GBR', inplace=True)
+    df_merged['NOC'].mask(df_merged['Country'] == 'Republic of Korea', 'KOR', inplace=True)
+    return df_merged
 
 
 if __name__ == '__main__':
-    prepare_data()
-    prepared_csv_filepath = Path(__file__).parent.parent.joinpath('data', 'paralympics_prepared.csv')
-    df_prepared = pd.read_csv(prepared_csv_filepath, parse_dates=['Start', 'End'], dtype={'Year': str})
+    df = prepare_data()
 
-    # Add code here to print the stats returned by describe
+    # A possible solution to the challenge, please share if you have a neater or more efficient solution!
+
+    # Check the data type of the columns
+    print("\ndata type of the columns\n", df[['Year', 'Start', 'End']].dtypes)
+
+    # Check the format of the strings in Start and End by printing a couple of rows
+    print(df[['Year', 'Start', 'End']].head(2))
+
+    # Add the year to the Start and End columns. Year is int and Start/End are strings so to combine as strings you
+    # need to first convert the Year to string
+    # TODO: Consider if there is a case where the dates span year end e.g. December to January)
+    df["Start"] = df["Start"] + '-' + df["Year"].astype(str)
+    df["End"] = df["End"] + '-' + df["Year"].astype(str)
+    print(df[['Year', 'Start', 'End']].head(2))
+    print(df[['Year', 'Start', 'End']].dtypes)
+
+    # Change the column datatype to date-time format
+    # Pandas to_datetime  handles most date formats so you can run the following without the format= and it will work
+    # date time formats are here for reference https://docs.python.org/3/library/datetime.html
+    df['Start'] = pd.to_datetime(df['Start'], format='%d-%b-%Y')
+    df['End'] = pd.to_datetime(df['End'])
+    print(df[['Year', 'Start', 'End']].head(2))
+    print(df[['Year', 'Start', 'End']].dtypes)
+
+    # Create a duration column that calculates days between the start and end
+    df['Duration'] = df['End'] - df['Start']
+    print(df.head(5))
+
+    # The output of the above is in timedelta format, however we want to compare duration as int
+    # Convert the format
+    print(df['Duration'].dtypes)
+    df['Duration'] = df['Duration'].dt.days.astype('int')
+    print(df['Duration'].dtypes)
+
+    # Save the prepared data to CSV
+    prepared_csv_filepath = Path(__file__).parent.parent.joinpath('data', 'paralympics_prepared.csv')
+    df.to_csv(prepared_csv_filepath, index=False)
